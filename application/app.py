f96108c9bc7acf20ac95f70103ca96da24655dcd
Cesare Placanica <cplacani@redhat.com>
2022-09-17 14:42:03 +0200

    remove duplication

--- application/app.py
+++ application/app.py
@@ -15,36 +15,31 @@ def home():
     return {"help": "This is a ping service"}
 
 
+def to_dict(ip, result, error_code):
+    """Returns the service data as dict"""
+    return {
+        "ip": ip,
+        "result": result,
+        "error_code": error_code,
+        "packets": PACKETS,
+    }
+
+
 @app.route("/ping/<string:ip>", methods=["GET"])
 def ping(ip):
     """Ping service"""
     try:
         socket.inet_aton(ip)
     except OSError:
-        return {
-            "ip": "na",
-            "result": "{} is not a valid IP address".format(ip),
-            "error_code": 0,
-            "packets": PACKETS,
-        }
-    p = subprocess.Popen(["ping", "-c", "5", ip])
+        return to_dict("na", "{} is not a valid IP address".format(ip), 0)
+    p = subprocess.Popen(["ping", "-c", str(PACKETS), ip])
     p.wait()
     error_code = p.poll()
     if error_code == 0:
-        return {
-            "ip": ip,
-            "result": "alive",
-            "error_code": error_code,
-            "packets": PACKETS,
-        }
+        return to_dict(ip, "alive", error_code)
     if error_code == 2:
-        return {
-            "ip": ip,
-            "result": "unreachable",
-            "error_code": error_code,
-            "packets": PACKETS,
-        }
-    return {"ip": ip, "result": "na", "error_code": error_code, "packets": PACKETS}
+        return to_dict(ip, "unreachable", error_code)
+    return to_dict(ip, "na", error_code)
 
 
 if __name__ == "__main__":
